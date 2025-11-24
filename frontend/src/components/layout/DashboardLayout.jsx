8c8bdc684edf2607e286469674d8120f894fdd10
PratikParate <paratepratik10@gmail.com>
2025-11-24 14:20:23 +0530

    add dashboard component

--- frontend/src/components/layout/DashboardLayout.jsx
+++ frontend/src/components/layout/DashboardLayout.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { Album, LogOut } from "Lucide-react";
+import { Link } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import Profiledropdown from "../layout/Profiledropdown";
 
-function DashboardLayout() {
+function DashboardLayout({ children }) {
+  const { user, logout } = useAuth();
+  const [ProfiledropdownOpen, setProfiledropdownOpen] = useState(false);
+
+  // Close Dropdown when Clicking outside
+  useEffect(() => {
+    const handleClickOutside = () => {
+      if (ProfiledropdownOpen) {
+        setProfiledropdownOpen(false);
+      }
+    };
+    document.addEventListener("click", handleClickOutside);
+    return () => document.removeEventListener("click", handleClickOutside);
+  }, [ProfiledropdownOpen]);
+
   return (
-    <div>DashboardLayout</div>
-  )
+    <div className="flex h-screen bg-gray-50">
+      <div className="flex-1 flex flex-col">
+        <header className="bg-white/80 backdrop-blur-sm border-b border-gray-200 h-16 flex items-center
+        justify-between px-6 sticky top-0 z-30">
+          <div className="flex items-center space-x-4">
+            <Link className="flex items-center space-x-3" to="/dashboard">
+              <div className="h-8 w-8 bg-gradient-to-br from-violet-400 to-violet-500 rounded-lg
+               flex items-center justify-center">
+                <Album className="h-5 w-5 text-white" />
+              </div>
+              <span className="text-black font-bold text-xl">AI ebook Creator</span>
+            </Link>
+          </div>
+          <div className="flex items-center space-x-3 ">
+            <Profiledropdown
+              isOpen={ProfiledropdownOpen}
+              onToggle={(e) => {
+                e.stopPropagation();
+                setProfiledropdownOpen(!ProfiledropdownOpen);
+              }}
+              avatar={user?.avatar || ""}
+              companyName={user?.name || ""}
+              email={user?.email || ""}
+              onLogout={logout}
+            />
+          </div>
+        </header>
+        <main className="flex-1 overflow-auto">{children}</main>
+      </div>
+    </div>
+  );
 }
 
-export default DashboardLayout
+export default DashboardLayout;
--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import toast from "react-hot-toast"
+import {Plus, Book} from "Lucide-react"
+
+import DashboardLayout from "../components/layout/DashboardLayout"
+import Button from "../components/ui/Button" 
+import { useAuth } from "../context/AuthContext"
+import axiosInstance from "../utils/axiosInstance"
+import { API_PATHS } from "../utils/apiPath"
+
 
 function DashboardPage() {
+  const [books, setBooks] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [isCreateModalOpen, setIsCreateModalOpen ] = useState(false)
+  const [bookToDelete, setBookToDelete] = useState(null)
+  const {user} = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    const fetchBooks = async () =>{
+      try{
+        const response = await axiosInstance.get(API_PATHS.BOOKS.GET_BOOK)
+        setBooks(response.data)
+      }catch(error){
+        toast.error("Failed to fetch your ebooks.")
+      }finally{
+        setIsLoading(false);
+      }
+    }
+  },[])
+
+  const handleDeleteBook = async () =>{
+    if(!bookToDelete) return;
+  }
+
+  const handleCreateBookClick = () =>{
+    setIsCreateModalOpen(true)
+  };
+    
+  const  handleBookCreated = (bookId) =>{
+    setIsCreateModalOpen(false)
+    navigate(`/editor/${bookId}`)
+  }
   return (
-    <div>DashboardPage</div>
+   <DashboardLayout>
+      <div className="container mx-auto p-6 ">
+        <div className="flex items-center justify-between mb-8 ">
+          <div>
+            <h1 className="text-lg font-bold text-slate-900">All eBooks</h1>
+            <p className="text-[13px] text-slate-500 mt-1">Create, edit , manage all your AI-generated ebooks.</p>
+          </div>
+          <Button
+          className="whitespace-nowrap"
+          onClick={handleCreateBookClick}
+          icon={Plus} >
+              Create New Books
+          </Button>
+        </div>
+      </div>
+   </DashboardLayout>
   )
-}
+  }
+
 
 export default DashboardPage
