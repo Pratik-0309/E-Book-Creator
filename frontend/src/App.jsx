9c2cd6b010d380e226d44d2354e09e0019c7ffd9
PratikParate <paratepratik10@gmail.com>
2025-11-24 11:39:16 +0530

    Refactor routing structure in App component to streamline ProtectedRoute usage and improve readability

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,6 +24,7 @@ function App() {
         <Route path='/dashboard' 
         element={<ProtectedRoute> <DashboardPage/> </ProtectedRoute>}
         />
+        
         <Route path='/editor/:bookId'
         element={<ProtectedRoute> <EditorPage/> </ProtectedRoute>}
         />
